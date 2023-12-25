dfc9a0d7d9b11fc8d622cb1fe6e986e2ac7a292d
Laurynas Lazauskas <8823448+lawrence-laz@users.noreply.github.com>
2023-12-25 10:39:56 +0200

    feat: .skip(usize)

--- src/iterator.zig
+++ src/iterator.zig
@@ -4,6 +4,7 @@ const WhereIterator = @import("where_iterator.zig").WhereIterator;
 const SelectIterator = @import("select_iterator.zig").SelectIterator;
 const ConcatIterator = @import("concat_iterator.zig").ConcatIterator;
 const TakeIterator = @import("take_iterator.zig").TakeIterator;
+const SkipIterator = @import("skip_iterator.zig").SkipIterator;
 
 pub fn Iterator(comptime TItem: type, comptime TImpl: type) type {
     return struct {
@@ -85,6 +86,18 @@ pub fn Iterator(comptime TItem: type, comptime TImpl: type) type {
         ) Iterator(TItem, TakeIterator(TItem, TImpl)) {
             const self_x = @constCast(&self.impl);
             return .{ .impl = TakeIterator(TItem, TImpl){
+                .prev_iter = self_x,
+                .index = 0,
+                .count = item_count,
+            } };
+        }
+
+        pub inline fn skip(
+            self: *const Self,
+            item_count: usize,
+        ) Iterator(TItem, SkipIterator(TItem, TImpl)) {
+            const self_x = @constCast(&self.impl);
+            return .{ .impl = SkipIterator(TItem, TImpl){
                 .prev_iter = self_x,
                 .index = 0,
                 .count = item_count,
@@ -253,4 +266,35 @@ test ".take() when iter is too short, stops at end" {
         index += 1;
     }
     try std.testing.expectEqual(@as(usize, 2), index);
+}
+
+test ".skip() when iter is long enough, takes after `item_count`" {
+    // Arrange
+    const numbers = &[_]u8{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    var iter = from.slice(u8, numbers);
+
+    // Act
+    var actual = iter.skip(3);
+
+    // Assert
+    const expected = &[_]u8{ 4, 5, 6, 7, 8, 9, 10 };
+    var index: usize = 0;
+    while (actual.next()) |actual_number| {
+        try std.testing.expectEqual(expected[index], actual_number);
+        index += 1;
+    }
+    try std.testing.expectEqual(@as(usize, 7), index);
+}
+
+test ".skip() when iter is too short, takes empty" {
+    // Arrange
+    const numbers = &[_]u8{ 1, 2 };
+    var iter = from.slice(u8, numbers);
+
+    // Act
+    var actual = iter.skip(3);
+
+    // Assert
+    const expected: ?u8 = null;
+    try std.testing.expectEqual(expected, actual.next());
 }
---
+++ src/skip_iterator.zig
@@ -0,0 +1,20 @@
+pub fn SkipIterator(
+    comptime TItem: type,
+    comptime TPrevIter: type,
+) type {
+    return struct {
+        const Self = @This();
+
+        prev_iter: *TPrevIter,
+        count: usize,
+        index: usize,
+
+        pub fn next(self: *Self) ?TItem {
+            while (self.index < self.count) {
+                _ = self.prev_iter.next();
+                self.index += 1;
+            }
+            return self.prev_iter.next() orelse null;
+        }
+    };
+}
