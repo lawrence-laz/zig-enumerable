0f04d8707071b410e31765b08b5b2911dbd0f804
Laurynas Lazauskas <8823448+lawrence-laz@users.noreply.github.com>
2024-02-06 23:02:14 +0200

    build: Add module

--- build.zig
+++ build.zig
@@ -15,10 +15,12 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
+    _ = b.addModule("enumerable", .{
+        .root_source_file = .{ .path = "src/enumerable.zig" },
+    });
+
     const lib = b.addStaticLibrary(.{
         .name = "enumerable",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
         .root_source_file = .{ .path = "src/enumerable.zig" },
         .target = target,
         .optimize = optimize,
